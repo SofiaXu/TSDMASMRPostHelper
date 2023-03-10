867567b31ee8074ba1b212f78fe864e9a36de4c5
Aoba <xuyaqi888@live.cn>
2023-03-10 14:40:28 +0800

    更新 0.2.0 版本
    1. 增加曲目表渲染
    2. 增加介绍渲染

--- code.user.js
+++ code.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         天使动漫 ASMR 版发帖辅助
 // @namespace    http://tampermonkey.net/
-// @version      0.1.2
+// @version      0.2.0
 // @description  天使动漫 ASMR 版发帖辅助
 // @author       Aoba Xu
 // @match        https://www.tsdm39.net/forum.php?mod=post&action=newthread&fid=581
@@ -14,8 +14,8 @@
 
 (function() {
     'use strict';
-    let titleTemplate = (rjCode, title, releaseDate, resolution, fileFormat) => `[DLsite自购][${rjCode}][${releaseDate}]${title}[${resolution}][${fileFormat}]`;
-    let bodyTemplate = (rjCode, coverUrl, title, circle, cv, releaseDate, resolution, fileFormat, tags, fileSize, introduction, trackList, selfCertUrl) => `[free]
+    const titleTemplate = (rjCode, title, releaseDate, resolution, fileFormat) => `[DLsite自购][${rjCode}][${releaseDate}]${title}[${resolution}][${fileFormat}]`;
+    const bodyTemplate = (rjCode, coverUrl, title, circle, cv, releaseDate, resolution, fileFormat, tags, fileSize, introduction, trackList, selfCertUrl) => `[free]
 购买原地址 Original Site：[url]https://www.dlsite.com/maniax/work/=/product_id/${rjCode}.html[/url]
 封面 Cover：
 [img]${coverUrl}[/img]
@@ -32,16 +32,18 @@
 文件容量 File Size：${fileSize}
 
 `
-/*
+    + (introduction ? `
 [spoiler=介绍原文 Introduction (Japanese)]
 ${introduction}
 [/spoiler]
+` : "")
+    + (trackList ? `
 [hr]
 [spoiler=曲目表 Track List]
 ${trackList}
 [/spoiler]
-*/
-+ `
+` : "")
+    + `
 [hr]
 本贴转载规则:
 Rules of Reshare
@@ -58,20 +60,49 @@ Rules of Reshare
 [img]${selfCertUrl === undefined || selfCertUrl === null ? "" : selfCertUrl}[/img]
 [/hide]
 `;
+    const renderTrackList = (doc) => {
+        const trackListElements = doc.querySelectorAll(".work_tracklist_item");
+        if (!trackListElements || trackListElements.length == 0) {
+            return undefined;
+        }
+        return `[list]
+${Array.from(trackListElements).map((x, i) => "[*]" + (i + 1) + ". " + x.querySelector(".title").innerText.trim() + " " + x.querySelector(".time").innerText.trim()).join("\n")}
+[/list]`;
+    };
+    const renderIntroduction = (doc) => {
+        const children = doc.querySelectorAll(".work_parts");
+        const result = Array.from(children).map((x) => {
+            x.classList.remove("work_parts");
+            switch (x.className) {
+                case "type_text":
+                    return `[size=5]${x.querySelector(".work_parts_heading").innerText.trim()}[/size]\n${x.querySelector(".work_parts_area").innerText.trim()}`;
+                case "type_image":
+                    return `[size=5]${x.querySelector(".work_parts_heading").innerText.trim()}[/size]
+[img]${x.querySelector(".work_parts_area.type_visual > .work_parts_multitype > .work_parts_multitype_item img").src}[/img]
+${x.querySelector(".work_parts_area.type_visual > .work_parts_multitype > .work_parts_multitype_item.type_text").innerText.trim()}`;
+                case "type_multiimages":
+                    return `[size=5]${x.querySelector(".work_parts_heading").innerText.trim()}[/size]\n` + Array.from(x.querySelectorAll(".work_parts_multiimage_item")).map(x => `[img]${x.querySelector("img").src}[/img]\n${x.querySelector(".text").innerText.trim()}`).join("\n");
+                default:
+                    return undefined;
+            }
+        }).filter(x => x)
+        return result.length > 0 ? result.join("\n[hr]\n") : undefined;
+    };
     document.getElementById("postbox").insertAdjacentHTML("afterbegin", `<div class="pbt cl">
     <div class="z"><span><input type="text" id="rj-number" style="min-width: " class="px" title="RJ号" placeholder="RJ号"/></span></div>
     <div class="z"><span><input type="text" id="resolution" class="px" title="解析度，格式：96kHz/24bit 或 320kbps" placeholder="解析度，格式：96kHz/24bit（无损） 或 320kbps（有损）"/></span></div>
     <div class="z"><span><input type="text" id="selfCertUrl" class="px" title="自购证明图片链接" placeholder="自购证明图片链接"/></span></div>
     <button type="button" class="pn pnc" id="gen-post">生成</button></div>`);
-    let rjBox = document.getElementById("rj-number");
-    let resolutionBox = document.getElementById("resolution");
-    let genPostButton = document.getElementById("gen-post");
-    let postArea = document.getElementById("e_textarea");
-    let titleBox = document.getElementById("subject");
-    let selfCertUrl = document.getElementById("selfCertUrl");
-    let fid = new URLSearchParams(location.search).get("fid");
+    const rjBox = document.getElementById("rj-number");
+    const resolutionBox = document.getElementById("resolution");
+    const genPostButton = document.getElementById("gen-post");
+    const postArea = document.getElementById("e_textarea");
+    const titleBox = document.getElementById("subject");
+    const selfCertUrl = document.getElementById("selfCertUrl");
+    const fid = new URLSearchParams(location.search).get("fid");
+    const imgUrlReg = /(?:\[img(?:\]|=\d+,\d+\])|)((?:http(?:s|):|)\/\/[-A-Za-z0-9+&@#/%?=~_|!:,.;]+\.(?:bmp|jpg|jpeg|gif|png|tif|tiff|svg|webp)(?:[-A-Za-z0-9+&@#/%?=~_|]+|))(?:\[\/img\]|)/gi;
     let parser = new DOMParser();
-    let keys = {
+    const keys = {
         "zh-cn": [
             "贩卖日",
             "声优",
@@ -97,12 +128,12 @@ Rules of Reshare
             "檔案容量"
         ]
     };
-    let nsfwKey = {
+    const nsfwKey = {
         "zh-cn": "18禁",
         "ja-jp": "18禁",
         "zh-tw": "18禁"
     };
-    let dateKey = {
+    const dateKey = {
         "zh-cn": "YYYY年MM月DD日",
         "ja-jp": "YYYY年MM月DD日",
         "zh-tw": "YYYY年MM月DD日"
@@ -123,7 +154,6 @@ Rules of Reshare
             url: `https://www.dlsite.com/maniax/work/=/product_id/${rjBox.value}.html`,
             onload: (r) => {
                 let doc = parser.parseFromString(r.responseText, "text/html");
-                console.log(doc);
                 let title = doc.getElementById("work_name").innerText;
                 let circle = doc.querySelector("#work_maker .maker_name a").innerText;
                 let coverUrl = doc.querySelector(".product-slider-data div").dataset.src;
@@ -144,12 +174,47 @@ Rules of Reshare
                 if (noResolution) {
                     resolution = /(wav|flac|ape|tak)/i.test(fileFormat) ? "44.1kHz/16bit" : "320kbps";
                 }
+                let description = doc.querySelector(".work_parts_container");
+                let trackList = renderTrackList(description);
+                let introduction = renderIntroduction(description);
                 //(rjCode, coverUrl, title, circle, cv, releaseDate, resolution, fileFormat, tags, fileSize, introduction, trackList, selfCertUrl)
-                postArea.value = bodyTemplate(rjBox.value, coverUrl, title, circle, cv, releaseDate.format("YYYY.MM.DD"), resolution, fileFormat, tags, fileSize, undefined, undefined, selfCertUrl.value);
+                postArea.value = bodyTemplate(rjBox.value, coverUrl, title, circle, cv, releaseDate.format("YYYY.MM.DD"), resolution, fileFormat, tags, fileSize, introduction, trackList, selfCertUrl.value);
                 //(rjCode, title, releaseDate, resolution, fileFormat)
                 titleBox.value = titleTemplate(rjBox.value, title, releaseDate.format("YYMMDD"), resolution, fileFormatSingle);
-                genPostButton.disabled = undefined;
-                genPostButton.innerText = "生成";
+                let imgCount = 0;
+                postArea.value.replace(imgUrlReg, (all, imgUrl) => {
+                    console.log(all, imgUrl)
+                    imgCount++;
+                    var readImage = new Image();
+                    readImage.onload = () => {
+                        var h = readImage.height;
+                        var w = readImage.width;
+                        if (w > 712) {
+                            h = Math.round(readImage.height / (w / 712));
+                            w = 712;
+                        }
+                        postArea.value = postArea.value.replace(all, `[img=${w},${h}]${readImage.src}[/img]`);
+                        imgCount--;
+                        if (imgCount == 0) {
+                            genPostButton.disabled = undefined;
+                            genPostButton.innerText = "生成";
+                        }
+                    };
+                    readImage.onerror = () => {
+                        if (readImage.src.match("https:")) {
+                            readImage.src = readImage.src.replace("https:", "http:");
+                        }
+                        else {
+                            imgCount--;
+                            if (imgCount == 0) {
+                                genPostButton.disabled = undefined;
+                                genPostButton.innerText = "生成";
+                            }
+                        }
+                    };
+                    readImage.src = imgUrl;
+                    return "";
+                });
             }
         })
     })
